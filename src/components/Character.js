929863d5be754b93c0799c0b27d287b97eeb6dde
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-28 15:44:33 -0800

    added character component

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Character() { const character = {
-    name, 
-    status, 
-    species, 
-    gender, 
-    origin, 
-    lastLocation,
-    image
-  }
+export default class Character extends Component{
+    static propTypes = {
+      name: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      species: PropTypes.string.isRequired,
+      gender: PropTypes.string.isRequired,
+      origin: PropTypes.string.isRequired,
+      lastLocation: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired
+    }
+    render() {
+      const { name, status, species, gender, origin, lastLocation, image } = this.props;
+      return (
+        <>
+          <div id='chars'>
+            <p>Name: {name}</p>
+            <p>Status: {status}</p>
+            <p>Species: {species}</p>
+            <p>Gender: {gender}</p>
+            <p>Origin: {origin}</p>
+            <p>Last Location: {lastLocation}</p>
+            <img href={image}></img>
+          </div>
+        </>
+      );
+    }
 }
+
+
