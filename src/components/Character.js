5e36f031e16f2b2cdcc699201302d3d6fcd5c509
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-12 13:51:37 -0800

    linting fixes

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,37 +3,40 @@ import PropTypes from 'prop-types';
 import stylesBanana from './Character.css';
 
 export default class Character extends Component{
-    static propTypes = {
-      name: PropTypes.string.isRequired,
-      status: PropTypes.string.isRequired,
-      species: PropTypes.string.isRequired,
-      gender: PropTypes.string.isRequired,
-      origin: PropTypes.string.isRequired,
-      lastLocation: PropTypes.string.isRequired,
-      image: PropTypes.string.isRequired
-    }
-    render() {
-      const { name, status, species, gender, origin, lastLocation, image } = this.props.data;
-
-      
-      return (
-        <>
-          <section className={stylesBanana.container}>
-            <div className={stylesBanana.information}>
-              <p>
-                <img src={image}></img>
-              </p>
-              <p>Name: {name}</p>
-              <p>Status: {status}</p>
-              <p>Species: {species}</p>
-              <p>Gender: {gender}</p>
-              <p>Origin: {origin}</p>
-              <p>Last Location: {lastLocation}</p>
-            </div>
-          </section>
-        </>
-      );
-    }
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    status: PropTypes.string.isRequired,
+    species: PropTypes.string.isRequired,
+    gender: PropTypes.string.isRequired,
+    origin: PropTypes.string.isRequired,
+    lastLocation: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+  }
+  
+  render() {
+    const { name, status, species, gender, origin, lastLocation, image } = this.props;
+    
+    
+    
+    
+    return (
+      <>
+        <section className={stylesBanana.container}>
+          <div className={stylesBanana.information}>
+            <p>
+              <img src={image}></img>
+            </p>
+            <p>Name: {name}</p>
+            <p>Status: {status}</p>
+            <p>Species: {species}</p>
+            <p>Gender: {gender}</p>
+            <p>Origin: {origin}</p>
+            <p>Last Location: {lastLocation}</p>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
 
